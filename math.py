642095d2c4fee67d2d7bcbf8e68806c355f6c0f3
niraJKuMAR789 <86824155+niraJKuMAR789@users.noreply.github.com>
2021-07-04 11:35:08 +0530

    headers in hub

--- math.py
+++ math.py
@@ -5,10 +5,10 @@ def add(x,y):
 #eurvurviegvmaindibey
 def subtract(x,y):
     return x-y          
-    
+#vhbvbaeirvb    
 def multiply(x,y):
     return x*y  
-    
+  #bf varkvh   
 def divide(x,y):
     return x/y
 
